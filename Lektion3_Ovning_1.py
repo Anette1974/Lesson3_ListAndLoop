b974856d224e05157faf9a6a6c728bb96b57b8c8
Anette1974 <anette.cedergren@yahoo.se>
2025-01-20 19:31:49 +0100

    Fortsättning övning 1

--- Lektion3_Ovning_1.py
+++ Lektion3_Ovning_1.py
@@ -1,2 +1,8 @@
 #https://github.com/Anette1974/Lesson3_ListAndLoop.git
 print("Övning 1, Diskutera i grupp")
+print ("del 1, Vad skrivs ut?")
+limit = 15
+index = 5
+while index <= limit:
+    print(index)
+    index = index + 2
